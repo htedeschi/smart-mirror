377df05070b1d2d5182cb071792c650c71959ddb
Henrique Tedeschi <htedeschi@Melaleuca.com>
2019-01-04 12:03:22 -0700

    New Mirror layout created

---
+++ public/js/new-mirror.js
@@ -0,0 +1,12 @@
+function ready() {
+    // set link
+    var link = document.getElementById('activate-link');
+    link.innerText = window.location.host + '/activate';
+
+    // get a new mirror code
+
+    // display mirror code
+
+    // check if the new mirror was added
+    // if so, load the confs and redirect to /mirror
+}
